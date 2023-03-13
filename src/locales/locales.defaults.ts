dce50a1062dd2d055f225346e741baeab560b4ed
alowave223 <belofost22@gmail.com>
2023-03-13 23:49:40 +0200

    Apply translation

--- src/locales/locales.defaults.ts
+++ src/locales/locales.defaults.ts
@@ -1,9 +1,11 @@
 export enum Locales {
   EN = 'en-US',
   UA = 'ua-UA',
+  TR = 'tr-TR',
 }
 
 export const LOCALES = {
   [Locales.EN]: { value: Locales.EN, caption: 'English (US)' },
   [Locales.UA]: { value: Locales.UA, caption: 'Українська' },
+  [Locales.TR]: { value: Locales.TR, caption: 'Türkçe' },
 };
