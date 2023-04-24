f87921675b750c309fcd1ffd81ea8b38cd043090
xmarkgs <markozhyvotkov@gmail.com>
2023-04-24 07:50:12 -0600

    RegView template fix

--- src/views/RegView.vue
+++ src/views/RegView.vue
@@ -324,7 +324,7 @@ onUnmounted(() => {
           <div v-else>
             <span>{{ $t('register.note') }}</span>
             <p>{{ $t('register.note_message') }}</p>
-            <p>{{ $t('register.note_attention') }}</p>
+            <p class="reg__note-attention">{{ $t('register.note_attention') }}</p>
             <span class="reg__note-help">
               {{ $t('register.note_stuck') }}
               <RouterLink to="/faq" target="_blank">{{
@@ -339,10 +339,11 @@ onUnmounted(() => {
           <CheckmarkIcon></CheckmarkIcon>
           <div>
             <p v-html="$t('register.note_success')"></p>
+            <span v-html="$t('register.note_success_hint')"></span>
           </div>
         </div>
       </FormStep>
-      <div class="reg__stepper">
+      <div class="reg__stepper" v-if="!doVerifiedAnimation">
         <button
           @click="prevStep()"
           :disabled="step == 1"
@@ -384,7 +385,7 @@ onUnmounted(() => {
         class="reg__captcha_agreement"
       >
       </span>
-      <RouterLink class="reg__link" to="/">{{
+      <RouterLink v-if="step !== 3" class="reg__link" to="/">{{
         $t('register.already_have_account')
       }}</RouterLink>
     </form>
@@ -454,11 +455,11 @@ onUnmounted(() => {
     font-style: normal;
     font-weight: 600;
     font-size: 24px;
-    line-height: 33px;
   }
 
-  p:nth-child(3) {
+  .reg__note-attention {
     margin-top: 10px;
+    font-size: 18px;
     color: $red;
   }
 
@@ -487,6 +488,11 @@ onUnmounted(() => {
 
 .reg__note_verified {
   text-align: center;
+  color: $n-success;
+
+  .success-checkmark {
+    transform: scale(0.8);
+  }
 
   p {
     margin: 0;
@@ -494,6 +500,13 @@ onUnmounted(() => {
     font-weight: 600;
     font-size: 20px;
     line-height: 33px;
+  }
+
+  span {
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      color: $secondary;
   }
 }
 
